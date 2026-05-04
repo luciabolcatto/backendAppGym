a2ec21aca4090e3633e2f068c6c1fdcf732b9d00
Ismael <ismaelrasa@gmail.com>
2026-05-04 01:23:15 -0300

    solucion error mail

--- src/shared/services/emailService.ts
+++ src/shared/services/emailService.ts
@@ -1,12 +1,19 @@
 import nodemailer from 'nodemailer';
 
 // Configuración del transportador de email
+
 const transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
   auth: {
     user: process.env.EMAIL_USER || 'fitnessprimeok@gmail.com',
     pass: process.env.EMAIL_PASS || 'ihklojlxpyxznxnz',
   },
+  tls: {
+    
+    rejectUnauthorized: false
+  }
 });
 
 /**
